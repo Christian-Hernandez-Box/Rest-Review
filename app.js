229232f7004eabab59681dd5b80fb647c94eff0b
Christian-Hernandez-Box <chernandez4923@utexas.edu>
2024-08-06 22:06:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const session = require("express-session");
+const helmet = require("helmet");
+const { check, validationResult } = require("express-validator");
+
+const PORT = process.env.PORT || 4001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("trust proxy", 1);
+app.use(helmet());
+app.use(
+  session({
+    secret: "my-secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 300000000,
+      sameSite: "none",
+      httpOnly: true,
+      secure: true,
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post(
+  "/review",
+  [
+    check("email").isEmail(),
+    check("restaurant").notEmpty().blacklist("<>"),
+    check("rating").isNumeric(),
+    check("review").notEmpty().blacklist("<>"),
+  ],
+  (req, res) => {
+    var errors = validationResult(req).array();
+    console.log(`Errors found: ${JSON.stringify(errors)}`);
+    if (errors.length >= 1) {
+      res.redirect("/error");
+    } else {
+      console.log("Data was valid!");
+      res.redirect("/success");
+    }
+  }
+);
+
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.get("/error", (req, res) => {
+  res.render("error");
+});
+
+app.listen(PORT, () =>
+  console.log(`The server is listening at port: http://localhost:${PORT}`)
+);
